14aeb11932360c11f4f37eb91501a4ae26f389ad
ZoeNguyen2810 <miumiu2810201@gmail.com>
2024-08-04 02:30:53 +0700

    vid 5

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -1,33 +1,55 @@
 const { gql } = require('apollo-server')
 
 const typeDefs = gql`
-type User {
-  id : ID!
-  name : String!
-  userName : String!
-  age : Int!
-  nationality: String!
-  friends : [User]
-}
-type Movie {
-  id : ID!
-  name : String!
-  yearOfPublic : Int!
-  isInCinema : Boolean!
-}
-type Query {
-  users : [User!]!
-  user ( id : ID!) : User!
-  movies : [Movie!]!
-  movie ( name : String!) : Movie!
+  type User {
+    id: ID!
+    name: String!
+    username: String!
+    age: Int!
+    nationality: String!
+    friends: [User]
+    favoriteMovies: [Movie]
+  }
+  
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
+  }
+  
+  input CreateUserInput {
+    name: String!
+    username: String
+    age: Int!
+    nationality: String!
+  }
+  
+  enum Nationality {
+    CANADA,
+    BRAZIL,
+    INDIA,
+    GERMANY,
+    CHILE
+  }
+  
+  input UpdateUserInput {
+    id: ID!
+    userNameNew: String!
+  }
 
-}
-`
-// enum Nationality {
-// CANADA,
-// BRAZIL,
-// INDIA,
-// GERMANY,
-// CHILE
-// }
-module.exports = { typeDefs}
+  type Query {
+    users: [User!]!
+    user(id: ID!): User!
+    movies: [Movie!]!
+    movie(name: String!): Movie!
+  }
+
+  type Mutation {
+    createUsers(input: CreateUserInput!): User!,
+    updateUserName(input : UpdateUserInput!) : User!,
+    deleteUser ( id : Int!) : User,
+  }
+`;
+
+module.exports = { typeDefs }
